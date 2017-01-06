da652130d1398587b71433fb232b3947a87803c7
Matthew O'Brien <maobrien11235@gmail.com>
2017-01-06 01:40:03 -0600

    Second commit: Created .rproject and began coding.

---
+++ 00_Introduction/introduction_code.R
@@ -0,0 +1,8 @@
+#############################################
+#                                           #
+# Introductory Script                       #
+#                                           #
+# Purpose: Introductory chapter and begin-  #
+# -ning notes. May not contain actual code. #
+#                                           #
+#############################################
