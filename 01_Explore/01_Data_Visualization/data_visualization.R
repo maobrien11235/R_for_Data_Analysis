0d0d3cdb83a27f45977c0db2a9d4940f0aa1042d
Matthew O'Brien <maobrien11235@gmail.com>
2017-01-08 12:31:23 -0600

    08JAN2016: Honestly, I'm overcommitting, but I don't care. Started the visualization section of the EXPLORE chapter. Have made progress and am coding the practice sections.

---
+++ 01_Explore/01_Data_Visualization/data_visualization.R
@@ -0,0 +1,46 @@
+# 08JAN2016
+# Explore.Data_Visualization
+
+library(tidyverse)
+
+# Let's start with a simple question: 
+#   Do larger engines use more gas?
+
+# Our dataset
+mpg
+
+# First plot to see displ(as in, engine size)
+# and hwy(as in, highway miles per gallon)
+
+ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy))
+
+ggplot(data = mpg)
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = cyl, y = hwy))
+
+# Let's see into those higher mpg cars at the right of the graph
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ, y=hwy, color=class))
+
+# We could also size our datapoints according to class,
+# but that can muddy our graph and decrease legibility
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ, y=hwy, size=class))
+
+# We could also use class as the determinant of opacity, 
+# using the alpha aesthetic
+
+# Left
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ, y=hwy, alpha=class))
+
+# Right
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ, y=hwy, shape=class))
+
+
+
+
+
